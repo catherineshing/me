873d9a71651cff3d88b53442d20606ba3ee4e556
Catherine Shing <catherine.shing@reputation.com>
2016-02-04 16:11:23 -0800

    fixing home image for big screens

--- src/about/about.js
+++ src/about/about.js
@@ -8,7 +8,7 @@ angular.module('me.about', [])
 
             this.title = 'Front-End Developer';
 
-            this.description = 'I am a Bay Area native and UCSD alum. I have ' +
+            this.description = 'I am a Bay Area native and UC San Diego alum. I have ' +
                 (new Date().getFullYear() - 2012).toString() +
                 ' years of experience working with a variety of technologies like AngularJS, Node.js, Java, and PHP. When I\'m not coding, I enjoy hanging out with my dog Bernie, eating good food, and kicking butt in Fantasy Football.';
         }
