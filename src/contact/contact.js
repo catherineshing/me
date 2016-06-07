f6c4608a0bfae7faf43706ca30b6a845f6556508
Catherine Shing <catherine.shing@reputation.com>
2016-06-07 10:50:26 -0700

    fixing contact form

--- src/contact/contact.js
+++ src/contact/contact.js
@@ -14,7 +14,12 @@ angular.module('me.contact', [])
                 that.actionUrl = $sce.trustAsResourceUrl('https://docs.google.com/forms/d/1E1zYwFsiVTnXFLPQoyu_p34LUTRGdSQHut5km3eNjok/formResponse?' +
                     'entry.549975105=' + that.name +
                     '&entry.364966672=' + that.email +
+                    '&entry.1033360665=' + that.company +
                     '&entry.1159796349=' + that.message);
+            };
+
+            this.send = function() {
+                that.sent = true;
             };
         }
     ]);
