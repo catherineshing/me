856afb8fce16bf68bdbab9591876a688a3e3b68e
Catherine Shing <catherine.shing@reputation.com>
2016-07-21 12:18:40 -0700

    hide copy button for ios

--- src/about/about.js
+++ src/about/about.js
@@ -4,13 +4,15 @@
 angular.module('me.about', [])
 
     .controller('AboutController', [
-        function() {
+        '$window',
+        function($window) {
             this.title = 'Front-End Developer';
 
             this.description = 'I am a Bay Area native and UC San Diego alum. I have ' +
                 (new Date().getFullYear() - 2012).toString() +
                 ' years of experience working with a variety of technologies like AngularJS, Node.js, Java, and PHP. When I\'m not coding, I enjoy hanging out with my dog Bernie, eating good food, and kicking butt in Fantasy Football.';
 
+            this.isIos = /iPhone|iPad|iPod/i.test($window.navigator.userAgent);
             this.clipboardSupported = false;
         }
     ]);
